3a5dfde8203fe571f45ab1c492f053b7de7e02b2
M4R5 ᵐᵛᵖˡ <67886336+mvplID@users.noreply.github.com>
2022-05-23 15:38:51 +0700

    Update handle_start.py

--- bot/core/handlers/handle_start.py
+++ bot/core/handlers/handle_start.py
@@ -5,7 +5,7 @@ async def start_handler(message):
     user_id= message.sender_id
     chat_id= message.chat_id
     if user_id in get_val("ALLOWED_USERS") or chat_id in get_val("ALLOWED_CHATS") or user_id == get_val("OWNER_ID"):
-        msg = '''**Selamat datang di Mirror RClone Bot, Bot ini dapat membantumu mengunggah TG file ke penyimpanan awan ataupun sebaliknya; serta trasnfer antar penyimpanan awan.\n
+        msg = '''**Selamat datang di Mirror RClone Bot**, Bot ini dapat membantumu mengunggah TG file ke penyimpanan awan ataupun sebaliknya; serta trasnfer antar penyimpanan awan.\n
 '''
         await message.reply(msg)
     else:
